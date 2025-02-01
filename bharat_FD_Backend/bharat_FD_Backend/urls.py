59bc1eb95f728ba011cfd10090c3ae0a161cf1b2
TaushiqB <taushiqbalamurugan@gmail.com>
2025-02-01 12:39:50 +0530

    chore: Configure API URLs and routing

--- bharat_FD_Backend/bharat_FD_Backend/urls.py
+++ bharat_FD_Backend/bharat_FD_Backend/urls.py
@@ -18,8 +18,27 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from . import view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+
+    path('admin/', admin.site.urls),
+    
+    
+    path('api/', include('faqs.urls')),
+    
+   
+    path('i18n/', include('django.conf.urls.i18n')),  
 ]
+
+
+urlpatterns += i18n_patterns(
+
+    prefix_default_language=False
+)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
